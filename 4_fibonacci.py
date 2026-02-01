040438bf1cf1d49c88a83757efcf76f74be68a5e
dipalee <109855353+dipspatil20@users.noreply.github.com>
2026-02-01 17:48:54 +0530

    code added

---
+++ 4_fibonacci.py
@@ -0,0 +1,7 @@
+num = int(input("enter num"))
+a=0
+b=1
+for i in range(num):
+    print(a, end=" ")
+    a,b = b, a+b
+
---
+++ 5_reverse_string.py
@@ -0,0 +1,5 @@
+st= "hello"
+rev=""
+for i in st:
+    rev =i+rev
+print(rev)
